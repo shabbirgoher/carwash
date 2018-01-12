9e48ba22c1f2d3fc1ca72d62e509b2c442651e63
Shabbir Goher <shabbir.goher@gmail.com>
2018-01-12 17:49:08 +0530

    Playing with react antive

--- react-app/App.js
+++ react-app/App.js
@@ -1,14 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default class App extends React.Component {
+class Greeting extends Component {
   render() {
     return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+export default class App extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      // <View style={styles.container}>
+      //   <View ></View>
+      //   <Image source={pic} style={styles.image}/>
+      //   <Greeting name="User"/>
+      //   {/* <Blink text='I love to blink' />
+      //   <Blink text='Yes blinking is so great' />
+      //   <Blink text='Why did they ever take this out of HTML' />
+      //   <Blink text='Look at me look at me look at me' /> */}
+      // </View>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Done 1st change</Text>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
@@ -17,8 +57,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'center'
   },
+  image: {
+    width: 193,
+    height: 110
+  }
 });
