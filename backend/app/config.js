6cc55d7c60f0f6788a07d184c1dcbc4a493ad38f
Shabbir Goher <shabbir.goher@gmail.com>
2018-01-13 13:36:00 +0530

    Facebook login in the app

---
+++ backend/app/config.js
@@ -0,0 +1,6 @@
+export const facebook = {
+    clientID: '169403277001949',
+    clientSecret: 'df601d57ee6676119bd4490067bdaee4',
+    callbackURL: 'http://10.0.2.2:3000/auth/facebook/callback',
+    profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
