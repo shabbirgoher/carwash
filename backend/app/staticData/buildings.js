c0a7e8d8355ea318b43d7451f09de67e7733e322
Shabbir Goher <shabbir.goher@gmail.com>
2018-02-17 10:43:52 +0530

    Added building names provided by hussain

--- backend/app/staticData/buildings.js
+++ backend/app/staticData/buildings.js
@@ -1,6 +1,4 @@
 exports.list = [
-   "Building 1 from server",
-   "Building 2 from server",
-   "Building 3 from server",
-   "Building 4 from server"
+   "AL FARDAN PROPERTIES",
+   "AL FARDAN PROPERTIES"
 ]
