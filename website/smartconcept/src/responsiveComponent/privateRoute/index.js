d79288c8d5dd00a8378e89310f637e4c5754ef41
Shabbir Goher <shabbir.goher@gmail.com>
2018-04-05 22:10:15 +0530

    Verify expiry date in token

--- website/smartconcept/src/responsiveComponent/privateRoute/index.js
+++ website/smartconcept/src/responsiveComponent/privateRoute/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
+import decode from 'jwt-decode';
 
 import { AuthService } from "./../../services/authService";
-
+function isExpired(token){
+    try {
+        const decoded = decode(token);
+        if (decoded.exp > Date.now() / 1000) { // Checking if token is expired. N
+            return true;
+        }
+        else
+            return false;
+    }
+    catch (err) {
+        return false;
+    }
+}
 const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route
         {...rest}
         render={props =>
-            AuthService.isAuthenticated() ? (
+            isExpired(AuthService.isAuthenticated()) ? (
                 <Component {...props} />
             ) : (
                     <Redirect
